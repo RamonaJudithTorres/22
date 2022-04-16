f012e3c26837ec5f61e3fdf2145056a17683704c
RamonaJudithTorres <ramonablacktorres@gmail.com>
2022-04-16 18:57:20 -0500

    se agregaron tarjetas de analisis a la portada

---
+++ src/components/Analisis.js
@@ -0,0 +1,97 @@
+import { Card, Button } from "react-bootstrap";
+import pic3 from '../assets/foto1.jpg'
+
+export default function Analisis() {
+  return (
+    <div className="mt-5 mx-5 d-flex">
+      <div className="container-fluid">
+        <div className="row mb-5">
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row mb-5">
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+
+        <div className="row mb-5">
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="col-md-6 mb-md-0 mb-3">
+            <Card style={{ width: "40rem" }}>
+              <Card.Img variant="top" src={pic3} />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/views/Portada.js
+++ src/views/Portada.js
@@ -1,11 +1,16 @@
 import {useState, useEffect } from 'react'
 import Carrusel from '../components/Carrusel'
+import Footer from '../components/Footer'
+import Analisis from '../components/Analisis'
 
 export default function Portada(){
 
     return(
             <div >
-                <Carrusel/>       
+                <Carrusel/>  
+                <Analisis/>  
+
+                <Footer/>
             </div>
     )
 }
